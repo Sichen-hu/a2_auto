6260ed07c0e0a7be634dfb9319a6fd331861e8da
HeyHao <heyhao@HeyHaos-MacBook-Pro.local>
2019-07-30 17:31:30 +0800

    server debug

--- server_src/server_main.py
+++ server_src/server_main.py
@@ -1,5 +1,13 @@
 from server.a2_ml_server import a2_ml_server
 import sys
+import traceback
 
 device = sys.argv[1]
-a2_ml_server(device= device)
+try:
+    a2_ml_server(device=device)
+except Exception as e:
+    traces = traceback.format_exc()
+
+    with open("/tmp/server.log","a") as f:
+        f.writelines([str(traces),str(e)])
+        f.close()
